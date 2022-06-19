bf017092103b9c32642712d3cbb6f0eb1d63ff5e
Robert <Robert-Schmidt10112000@outlook.de>
2022-06-19 23:19:23 +0200

    Übung 9

--- Uebung_9/aufg_4.cpp
+++ Uebung_9/aufg_4.cpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <unordered_map>
 
+//einfachste Methode, da Konstruktion des pairs am ehesten Konstruktionen anderer Objekte wie Objekt einer Klasse ähnelt
 std::vector<std::pair<std::string, int>> create_vector(std::list<std::string> l, std::list<int> l2){
 std::list<std::string>::iterator i1 = l.begin();
 std::list<int>::iterator i2 = l2.begin();
@@ -15,6 +16,7 @@ while(i1 != l.end() && i2 != l2.end()){
 }
 return v;
 }
+
 
 std::vector<std::pair<std::string, int>> create_vector_2(std::list<std::string> l, std::list<int> l2){
 std::list<std::string>::iterator i1 = l.begin();
@@ -29,15 +31,13 @@ while(i1 != l.end() && i2 != l2.end()){
 return v;
 }
 
+// nicht einfach zu verstehen, weil Konstruktion des pairs hinter der Funktion versteckt ist
 std::vector<std::pair<std::string, int>> create_vector_3(std::list<std::string> l, std::list<int> l2){
 std::list<std::string>::iterator i1 = l.begin();
 std::list<int>::iterator i2 = l2.begin();
 std::vector<std::pair<std::string, int>> v;
-std::pair<std::string, int> p;
 while(i1 != l.end() && i2 != l2.end()){
-    p.first = (*i1);
-    p.second = (*i2);
-    v.push_back(p);
+    v.emplace_back(*i1, *i2);
     i1++;
     i2++;
 }
@@ -48,7 +48,7 @@ return v;
 int main() {
 std::list<std::string> l1 = {"hallo", "hi", "ho"};
 std::list<int> l2 = {34, 45, 56};
-std::vector<std::pair<std::string, int>> v = create_vector_3(l1, l2);
+std::vector<std::pair<std::string, int>> v = create_vector_2(l1, l2);
 for(auto it=v.begin(); it!=v.end(); it++){
     std::cout<<it->first<< "+" << it->second<< std::endl;
 }

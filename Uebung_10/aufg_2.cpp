b2169823a0ef515a870933c5c791fd54ab845a8f
Robert <Robert-Schmidt10112000@outlook.de>
2022-06-26 23:30:17 +0200

    Übung_10

--- Uebung_10/aufg_2.cpp
+++ Uebung_10/aufg_2.cpp
@@ -40,7 +40,37 @@ template <class RandomAccessIterator>
    while(help4 != last){
     //clamp(*help4,INTMAX_MIN, INTMAX_MAX);
     help5 = help4;
-    *help5 = std::clamp(*help4, 0, 12);
+    *help5 = std::clamp(*help4, 0, 100);
+    
+    
+    help4++;
+   }
+   while(help!= first){
+    help2 = first;
+    while(help2 != last){
+        help3 = help2;
+        help2++;
+        if(*help3>*help2 && help2 != last){
+            
+            std::iter_swap(help3, help2);
+        }
+    }
+    help--;
+   }
+  }
+
+  template <class RandomAccessIterator>
+  void sort_me_without_clamp (RandomAccessIterator first, RandomAccessIterator last){
+   RandomAccessIterator help = last;
+   RandomAccessIterator help2;
+   RandomAccessIterator help3;
+   RandomAccessIterator help4 = first;
+   RandomAccessIterator help5;
+   while(help4 != last){
+    //clamp(*help4,INTMAX_MIN, INTMAX_MAX);
+    *help4 = min(*help4, 100);
+    *help4 = max(*help4, 0);
+    
     
     
     help4++;
@@ -63,12 +93,11 @@ template <class RandomAccessIterator>
 
 int main() {
     
-std::vector<int> v = { 4, 18, 46, 36 , 67,4, 3, 57};
-std::cout << std::clamp(23, 0, 10) << std::endl;
+std::vector<int> v = { 4, 18, 46, 36 , 67,4, 3, 101};
 for (int i = 0; i < (int)v.size(); i++){
         std::cout<< v.at(i) <<std::endl;}
 
-sort_me_with_clamp  (v.begin(), v.end());
+sort_me_without_clamp  (v.begin(), v.end());
 for (int i = 0; i < (int)v.size(); i++){
         std::cout<< v.at(i) <<std::endl;}
 
--- Uebung_10/aufg_3.cpp
+++ Uebung_10/aufg_3.cpp
@@ -12,74 +12,61 @@ bool has_realtionship(Person &p){
     }
 }
 
-auto has_age = [](Person* p){ return p->getAge() == 21; };
-bool has_age2(Person* p){
-    std::cout<< "helllllllllllllllllllllllllllllllllllllllll";
-    if(p->getAge()==21){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-std::vector<Person> find_persons(Person &p){
-    std::vector<Person> returnq;
-    int n =0;
+
+std::vector<Person*> find_persons(Person &p, int year){
+    std::vector<Person*> returnq;
+    int n =1;
     std::vector<Person*> find1 = p.getParents();
     
+    auto has_age = [&](Person* p){ return p->getAge() == year; };
     auto help2= find1.begin();
-    for (int i = 0; i < (int)p.getParents().size(); i++){
-        std::cout<< p.getParents().at(i)->getAge() <<std::endl;}
+    
     while(n==1){
-    auto help= std::find_if(help2, find1.end(),  [](Person* const& c) { return has_age2(c); });
+ 
+    auto help= std::find_if(help2, find1.end(),  has_age);
     if(help != find1.end()){
-    returnq.push_back(**help);
+    returnq.push_back(*help);
     help++;
     help2 = help;
     }
     else{
-        n=1;
+        n=0;
     }
     }
-    n=0;
+    n=1;
     find1= p.getSiblings();
     help2= find1.begin();
     while(n==1){
     auto help= (std::find_if(help2, find1.end(), has_age));
     if(help != find1.end()){
-    returnq.push_back(**help);
+    returnq.push_back(*help);
     help++;
     help2 = help;
     }
     else{
-        n=1;
+        n=0;
     }
     }
 
-    n=0;
+    n=1;
     find1= p.getChildren();
     help2= find1.begin();
     while(n==1){
+
     auto help= (std::find_if(help2, find1.end(), has_age));
     if(help != find1.end()){
-    returnq.push_back(**help);
+    returnq.push_back(*help);
     help++;
     help2 = help;
     }
     else{
-        n=1;
+        n=0;
     }
     }
     if(p.getSpouse()->getAge()==21){
-        returnq.push_back(*p.getSpouse());
+        returnq.push_back(p.getSpouse());
     }
 
-    std::cout<< "hello";
-    if(returnq.empty()){
-        std::cout<< "hello";
-    }
-    for (int i = 0; i < (int)returnq.size(); i++){
-        std::cout<< returnq.empty() <<std::endl;}
     return returnq;
 
 }
@@ -89,7 +76,7 @@ std::vector<Person> find_persons(Person &p){
 int main() {
 Person carmen("Carmen", "Schmidt", 21);
 Person steffen("Steffen", "Schmidt", 21);
-Person juliane("Juliane", "Schmidt", 21);
+Person juliane("Juliane", "Schmidt", 24);
 Person kevin("Kevin", "Schwabe", 21);
 Person opa("helmut","Schmidt", 21);
 Person oma("Gerdrut", "Schmidt", 85);
@@ -105,10 +92,10 @@ steffen.addChildren(&juliane);
 
 Robert->addSibling(&juliane);
 std::cout<< has_realtionship(*Robert) <<std::endl;
-std::vector<Person> v = find_persons(steffen);
+std::vector<Person*> v = find_persons(steffen, 21);
 for (int i = 0; i < (int)v.size(); i++){
-        std::cout<< v.at(i).getName() <<std::endl;}
-std::cout<< has_age(Robert)<< std::endl;
+        std::cout<< v.at(i)->getName() <<std::endl;}
+
 
 return 0;
 }
